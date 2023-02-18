2814bb32323abbff887bcd303ad51c9fc7cdc820
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-02-18 18:59:35 +0300

    fix: fix cors options

--- src/components/authorization.ts
+++ src/components/authorization.ts
@@ -18,7 +18,7 @@ const hash = (string: string) => {
 
 function authorization() {
     const corsOptions = {
-        origin: '*',
+        origin: ['http://127.0.0.1:5500', 'http://127.0.0.1:8080', 'http://192.168.100.34:8080', 'http://localhost:8080/'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
         allowedHeaders: ['admin-pass']
